22464c888b23977f5b9e33d6946f4b75f0c19310
Marcelo Guazzardo <tercermundo19@gmail.com>
2025-03-29 04:31:10 -0300

    New branch with tests

---
+++ run_tests.py
@@ -0,0 +1,11 @@
+import unittest
+import sys
+
+if __name__ == "__main__":
+    # Discover and run all tests
+    test_suite = unittest.defaultTestLoader.discover('tests')
+    test_runner = unittest.TextTestRunner(verbosity=2)
+    result = test_runner.run(test_suite)
+    
+    # Exit with non-zero code if tests failed
+    sys.exit(not result.wasSuccessful())
